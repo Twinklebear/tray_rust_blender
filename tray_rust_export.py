0f219d83ed51cbc314a9a30969faba199843c01c
Will Usher <willusher.life@gmail.com>
2015-12-21 20:12:22 -0800

    Some code clean up: push animation export into a function

--- tray_rust_export.py
+++ tray_rust_export.py
@@ -4,11 +4,47 @@ import mathutils
 import json
 import re
 
+TRANSFORM_MAT = mathutils.Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+
 # Convert a matrix from Blender's coordinate system to tray_rust's
 def convert_blender_matrix(mat):
-    transform_mat = mathutils.Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-    return mathutils.Matrix.Scale(-1, 4, [1, 0, 0]) * transform_mat.inverted() * mat \
-        * transform_mat * mathutils.Matrix.Rotation(math.radians(90), 4, "X")
+    return mathutils.Matrix.Scale(-1, 4, [1, 0, 0]) * TRANSFORM_MAT.inverted() * mat \
+        * TRANSFORM_MAT * mathutils.Matrix.Rotation(math.radians(90), 4, "X")
+
+# Convert a matrix from Blender's OBJ export coordinate system to tray_rust's
+def convert_obj_matrix(mat):
+    return mathutils.Matrix.Scale(-1, 4, [1, 0, 0]) * TRANSFORM_MAT.inverted() * mat * TRANSFORM_MAT
+
+# Sample and export the keyframes of the object's animation to a dict for saving to the scene file
+# mat_convert is a function that will convert the object's matrix to tray_rust's coordinate
+# system. Returns the dict of control points, knots and degree required to specify the curve
+# for the animation in the scene file
+def export_animation(obj, mat_convert):
+    frame_time = 1.0 / scene.render.fps
+    knots = []
+    control_points = []
+    anim_data = obj.animation_data
+    start = int(anim_data.action.frame_range[0])
+    end = int(math.ceil(anim_data.action.frame_range[1]))
+    knots.append(start)
+    for f in range(start, end + 1):
+        scene.frame_set(f)
+        mat = mat_convert(obj.matrix_world)
+        knots.append(f * frame_time)
+        control_points.append({
+            "transform": [
+                {
+                    "type": "matrix",
+                    "matrix": [mat[0][0:], mat[1][0:], mat[2][0:], mat[3][0:]]
+                }
+            ]})
+    knots.append(end)
+    scene.frame_set(1)
+    return {
+        "control_points": control_points,
+        "knots": knots,
+        "degree": 1
+    }
 
 filepath = "C:/Users/Will/Desktop/"
 
@@ -50,34 +86,7 @@ camera_json = {
 }
 
 if camera.animation_data and camera.animation_data.action:
-    print("Animation data for {} = {}".format("Camera", camera.animation_data))
-    print("frame range = {}".format(camera.animation_data.action.frame_range))
-    frame_time = 1.0 / scene.render.fps
-    knots = []
-    control_points = []
-    anim_data = camera.animation_data
-    start = int(anim_data.action.frame_range[0])
-    end = int(math.ceil(anim_data.action.frame_range[1]))
-    knots.append(start)
-    for f in range(start, end + 1):
-        scene.frame_set(f)
-        cam_mat = convert_blender_matrix(camera.matrix_world)
-        knots.append(f * frame_time)
-        control_points.append({
-            "transform": [
-                {
-                    "type": "matrix",
-                    "matrix": [cam_mat[0][0:], cam_mat[1][0:], cam_mat[2][0:], cam_mat[3][0:]]
-                }
-            ]})
-    knots.append(end)
-
-    scene.frame_set(1)
-    camera_json["keyframes"] = {
-            "control_points": control_points,
-            "knots": knots,
-            "degree": 1
-        }
+    camera_json["keyframes"] = export_animation(camera, convert_blender_matrix)
 else:
     cam_mat = convert_blender_matrix(camera.matrix_world)
     camera_json["transform"] = [
@@ -128,40 +137,10 @@ for name, obj in scene.objects.items():
 
         mesh_transforms[name] = obj.matrix_world.copy()
         # Note: We don't perform the X rotation on meshes because they get rotated when exporting to the OBJ file
-        transform_mat = mathutils.Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
         if obj.animation_data and obj.animation_data.action:
-            print("Animation data for {} = {}".format(name, obj.animation_data))
-            print("frame range = {}".format(obj.animation_data.action.frame_range))
-            frame_time = 1.0 / scene.render.fps
-            knots = []
-            control_points = []
-            anim_data = obj.animation_data
-            start = int(anim_data.action.frame_range[0])
-            end = int(math.ceil(anim_data.action.frame_range[1]))
-            knots.append(start)
-            for f in range(start, end + 1):
-                scene.frame_set(f)
-                obj_mat = mathutils.Matrix.Scale(-1, 4, [1, 0, 0]) * transform_mat.inverted() \
-                        * obj.matrix_world * transform_mat
-                knots.append(f * frame_time)
-                control_points.append({
-                    "transform": [
-                        {
-                            "type": "matrix",
-                            "matrix": [obj_mat[0][0:], obj_mat[1][0:], obj_mat[2][0:], obj_mat[3][0:]]
-                        }
-                    ]})
-            knots.append(end)
-
-            scene.frame_set(1)
-            objects[-1]["keyframes"] = {
-                    "control_points": control_points,
-                    "knots": knots,
-                    "degree": 1
-                }
+            objects[-1]["keyframes"] = export_animation(obj, convert_obj_matrix)
         else:
-            obj_mat = mathutils.Matrix.Scale(-1, 4, [1, 0, 0]) * transform_mat.inverted() \
-                    * obj.matrix_world * transform_mat
+            obj_mat = convert_obj_matrix(obj.matrix_world)
             objects[-1]["transform"] = [
                     {
                         "type": "matrix",
@@ -228,7 +207,7 @@ for name, obj in scene.objects.items():
                 "type": "emitter",
                 "material": "white_wall",
                 "emitter": "area",
-                "emission": [0.780131, 0.780409, 0.775833, 100],
+                "emission": [0.780131, 0.780409, 0.775833, 50],
                 "geometry": lamp_geometry,
                 "transform": [
                     {
