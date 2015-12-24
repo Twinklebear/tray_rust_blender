5fa9c3618e9c9ccf2c0bfd512358fe381e882f94
Will Usher <willusher.life@gmail.com>
2015-12-24 10:29:08 -0800

    Remove debugging setting of not appending the menu each time

--- tray_rust_export.py
+++ tray_rust_export.py
@@ -301,7 +301,7 @@ def menu_func(self, context):
 
 def register():
     bpy.utils.register_module(__name__)
-    #bpy.types.INFO_MT_file_export.append(menu_func)
+    bpy.types.INFO_MT_file_export.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
